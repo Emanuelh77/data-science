f00abb82c03d9929498fcbe09d835d47535b6a4a
Emanuel Hernandez <emanuelh77@hotmail.com>
2018-12-08 15:35:03 -0700

    created chatbot using NLTK library, not fully functional

--- chatbot-ROBO.py
+++ chatbot-ROBO.py
@@ -24,7 +24,7 @@ lemmer = nltk.stem.WordNetLemmatizer() #wordnet is a semantically-oriented Engli
 #Pre-processing raw data
 
 def lem_tokens(tokens):
-    return [lemmer.lemmatize(token) for token in tokens]
+    return [lemmer.lemmatize(token) for token in tokens] 
 remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
 
 def lem_normalize(text):
@@ -41,3 +41,41 @@ def greeting(sentence):
 
 #Generating a response
 
+
+def response(user_response):
+    robo_response = ''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=lem_normalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx = vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf == 0):
+        robo_response = robo_response+"I am sorry! I don't understand you"
+        return robo_response
+    else:
+        robo_response = robo_response+sent_tokens[idx]
+        return robo_response
+
+
+flag = True
+print("ROBO: My name is Robo. I will answer your queries about anything. If you want to exit, type Bye!")
+while(flag == True):
+    user_response = input()
+    user_response = user_response.lower()
+    if(user_response != 'bye'):
+        if(user_response == 'thanks' or user_response == 'thank you'):
+            flag = False
+            print("ROBO: You are welcome..")
+        else:
+            if(greeting(user_response) != None):
+                print("ROBO: "+greeting(user_response))
+            else:
+                print("ROBO: ", end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag = False
+        print("ROBO: Bye! take care..")
